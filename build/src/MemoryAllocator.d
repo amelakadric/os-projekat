ce38baed093b8eb9dc74b9246c6712f136c3d1e4
OS Kernel <amela00108@gmail.com>
2022-08-26 12:24:13 -0700

    added syscall_c.h

--- build/src/MemoryAllocator.d
+++
@@ -1,4 +0,0 @@
-build/src/MemoryAllocator.o: src/MemoryAllocator.cpp \
- src/../h/MemoryAllocator.hpp
-
-src/../h/MemoryAllocator.hpp:
--- build/src/main.d
+++
@@ -1,3 +0,0 @@
-build/src/main.o: src/main.c src/../lib/console.h
-
-src/../lib/console.h:
